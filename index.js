2ecca0cd470ad20c8e18c1ee01aa887a7d989ac4
Edelgard <jtboyman@gmail.com>
2021-12-03 17:57:50 -0500

    can add multiple employees and all saved to array as objects

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const Builder = require('./lib/Builder');
+const {writeFile, copyFile} = require('./utils/generate-site')
+const generatePage = require('./src/generate-page')
 
 new Builder().initializeBuilder();
